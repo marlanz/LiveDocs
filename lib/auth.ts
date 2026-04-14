00063598ebded1f04f2844540595bbc0527b15ed
marlanz <phucanhdodang1211@gmail.com>
2026-04-15 04:49:53 +0700

    feat: add better-auth configuration

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { nextCookies } from "better-auth/next-js";
 
 const client = new MongoClient(process.env.MONGODB_URI!);
 const db = client.db();
@@ -10,4 +11,20 @@ export const auth = betterAuth({
     // Optional: if you don't provide a client, database transactions won't be enabled.
     client,
   }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 60 * 60 * 24,
+    },
+  },
+  plugins: [nextCookies()],
+  //   socialProviders: {
+  //     github: {
+  //       clientId: process.env.GITHUB_CLIENT_ID as string,
+  //       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+  //     },
+  //   },
 });
